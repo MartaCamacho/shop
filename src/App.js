b07ecf7915655f43729b8f74896d38ce116cf41c
martacamacho <martacamachovargas@gmail.com>
2023-06-27 19:28:31 +0200

    implemented react router and created routes and views

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './routes/root';
+import Product from './routes/product';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "product/:productId",
+    element: <Product />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      hola
+    holi
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/routes/root.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Root = () => {
+  return (
+    <div>eee</div>
+  )
+}
+
+export default Root;
