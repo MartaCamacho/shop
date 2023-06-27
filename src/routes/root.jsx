2576b2507db27eb20d074cc63ebe064a2a1744da
martacamacho <martacamachovargas@gmail.com>
2023-06-27 20:20:54 +0200

    implemented API call and filter

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 const Root = () => {
+    const [products, setProducts] = useState(null);
+    const [filteredProducts, setFilteredProducts] = useState(null);
+    const [search, setSearch] = useState(null);
+
+    useEffect(() => {
+      axios.get("https://itx-frontend-test.onrender.com/api/product")
+      .then(response => {
+        setProducts(response.data);
+        console.log(response.data)
+      })
+      .catch(error => console.error(error));
+    }, []);
+
+    useEffect(() => {
+        if(products) {
+            const productsToFilter = [...products];
+            const filtered = productsToFilter.filter(product => product.brand.toLowerCase().includes(search) || product.model.toLowerCase().includes(search));
+            setFilteredProducts(filtered);
+        }
+    }, [search])
+    
+
+    const productsToRender = filteredProducts ? filteredProducts : products;
+
   return (
-    <div>eee</div>
+    <div>
+        <input type="text" onChange={(event) => setSearch(event.target.value)}/>
+        {productsToRender ? productsToRender.map(product => {
+            return <div>
+                        <div>{product.brand}</div>
+                        <div>{product.model}</div>
+                    </div>
+        }) : <></>}
+    </div>
   )
 }
 
